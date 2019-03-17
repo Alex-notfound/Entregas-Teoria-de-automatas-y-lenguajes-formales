7b86340aabebd0d4f14c96d1d09be4b394342e02
Alex-notfound <curras.alex@hotmail.com>
2019-03-17 23:25:34 +0100

    Create ejecutar.sh

---
+++ Entrega 1/ejecutar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm salida.txt
+clear
+flex especificacionAnalizador.l
+gcc -o especificacionAnalizador lex.yy.c diccionario.c
+./especificacionAnalizador prueba.txt salida.txt
+cat salida.txt
+echo ""
+echo ""
+
