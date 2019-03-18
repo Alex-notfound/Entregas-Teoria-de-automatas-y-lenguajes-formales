da30974ab2d9825912cacf2e119b47dea1b02db0
Alex-notfound <curras.alex@hotmail.com>
2019-03-18 01:54:32 +0100

    Update ejecutar.sh

--- Entrega 1/ejecutar.sh
+++ Entrega 1/ejecutar.sh
@@ -4,4 +4,5 @@ clear
 flex especificacionAnalizador.l
 gcc -o especificacionAnalizador lex.yy.c diccionario.c
 ./especificacionAnalizador prueba.txt >> salida.txt
-cat salida.txt
+#cat salida.txt
+sdiff salida.txt salidaObjetivo.txt
