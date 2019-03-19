35c9aef855562f21e5caf5e2a97c5d7bb3e42bb6
Alex-notfound <curras.alex@hotmail.com>
2019-03-19 15:59:16 +0100

    Update ejecutar.sh

--- Entrega 1/ejecutar.sh
+++ Entrega 1/ejecutar.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 rm salida.txt
+#Realiza varios saltos de linea
 clear
+#Compila la especificacion del analizador y genera un fichero C
 flex especificacionAnalizador.l
+#Compila ficheros C
 gcc -o especificacionAnalizador lex.yy.c diccionario.c
+#Ejecuta
 ./especificacionAnalizador prueba.txt >> salida.txt
+#Muestra la salida almacenada en salida.txt
 #cat salida.txt
+#Sirve para comparar las diferencias entre el fichero de salida y el fichero con la salida esperada
 sdiff salida.txt salidaObjetivo.txt
